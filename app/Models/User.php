6758379ffe1816198f8d6fa551226b7b7f1fde3d
liudi <297114195@qq.com>
2020-11-11 14:53:27 +0000

    消息通知回复

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,29 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailCOntract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEamilTrait;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable,MustVerifyEamilTrait;
+    // use Notifiable,MustVerifyEamilTrait;
+    use MustVerifyEamilTrait;
+
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    public function notify($instance)
+    {
+        if ($this->id == Auth::id()) {
+            return;
+        }
+
+        if (method_exists($instance, 'toDatabase')) {
+            $this->increment('notification_count');
+        }
+
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -13,11 +14,12 @@ class ReplyObserver
     {
         $reply->content = clean($reply->content, 'user_topic_body');
     }
+
     public function created(Reply $reply)
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
-        // $reply->topic->increment('reply_count', 1);
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
